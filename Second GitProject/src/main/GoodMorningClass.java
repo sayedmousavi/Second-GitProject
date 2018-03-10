37a7e7b3aed3478be88039188428f81d6a8a8726
sayedmousavi <mohammadmousavi816@gmail.com>
2018-03-10 11:45:51 -0500

    Start

---
+++ Second GitProject/src/main/GoodMorningClass.java
@@ -0,0 +1,11 @@
+package main;
+
+public class GoodMorningClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Good mORNING");
+
+	}
+
+}
